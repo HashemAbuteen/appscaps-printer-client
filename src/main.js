29e509e36e4d5e1aee0a19d0c0b548bcbada5651
Hashem <hashemabualteen@gmail.com>
2024-09-08 17:16:47 +0300

    revert

--- src/main.js
+++ src/main.js
@@ -159,6 +159,9 @@ const printOrderWithOrderObject = (newOrder) => {
                         margin-bottom: ${userMargin.bottom || 0}mm;
                         margin-left: ${userMargin.left || 0}mm;
                         padding: 0;
+                        position: absolute;
+                        left: 0;
+                        top: 0;
                         width: ${paperSize || '80mm'};
                         padding: 5mm;
                     }
