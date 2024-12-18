034143f93939e2cdb355b2cce2f1668b2785dc4a
Hashem <hashemabualteen@gmail.com>
2024-12-18 17:30:40 +0200

    add build for ia32

--- build.js
+++ build.js
@@ -2,11 +2,8 @@ require('dotenv').config();
 const { execSync } = require('child_process');
 
 try {
-    console.log('Building for 64-bit Windows...');
-    execSync('npx electron-builder --win --x64 --publish always', { stdio: 'inherit' });
-
-    console.log('Building for 32-bit Windows...');
-    execSync('npx electron-builder --win --ia32 --publish always', { stdio: 'inherit' });
+    console.log('Building...');
+    execSync('npx electron-builder --win --publish always', { stdio: 'inherit' });
 } catch (error) {
     console.error('Error during the build process:', error);
     process.exit(1);
